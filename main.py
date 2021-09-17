0408054cf337508335b3ee68be7d587e2d225f81
ChanifRusydi <62838892+ChanifRusydi@users.noreply.github.com>
2021-09-17 17:06:41 +0700

    initial progress

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import discord
+from discord.ext import commands
+from youtube_dl import YoutubeDL
 
+class music(commands.music):
+    def __init__(self,bot):
+        self.bot=bot
+
+        self.is_play=False
